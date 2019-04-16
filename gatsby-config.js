37727b6b2c5e06d52f4eb8bbe5f476439e6173b2
Alex Welcing <alexwelcing@gmail.com>
2019-04-16 11:13:23 -0400

    change the package

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Ventures`,
+    author: `Laurlex`,
+    description: `Keeping outside of ordinary.`,
+    siteUrl: `https://laurlex.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `alexwelcing`,
     },
   },
   plugins: [
